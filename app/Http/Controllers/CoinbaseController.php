4b3f3374c11cdca5788527393f7b720146ac28e4
Matheus Reis <theus.h.ass.reis@gmail.com>
2019-11-22 13:59:56 -0300

    Some tests and new routes

--- app/Http/Controllers/CoinbaseController.php
+++ app/Http/Controllers/CoinbaseController.php
@@ -10,22 +10,32 @@ class CoinbaseController extends Controller
 {
     private $api_key;
     private $api_secret;
-    private $base_url = 'https://api.pro.coinbase.com';
     private $configuration;
     private $client;
 
     public function __construct() {
         # Chaves default
-        $this->api_key = env('COINBASE_API_KEY');
-        $this->api_secret = env('COINBASE_SECRET');
-        $this->passphrase = env('COINBASE_PASSPHRASE');
+        // $this->api_key = env('COINBASE_API_KEY');
+        // $this->api_secret = env('COINBASE_SECRET');
+        // $this->passphrase = env('COINBASE_PASSPHRASE');
 
         # Chaves sandbox
-        // $this->api_key = '010e7508032e404c54ca850f8da4ea26';
-        // $this->api_secret = 'ek/fvO7YanVTgU6shT6eljUaGBQU3VB6ERv1ZfmPYjrTvqUXgg7s8Ng9mOkuuk3rzz7KRbTuyrW7lQNf/8IbGw==';
-        // $this->passphrase = 'ngucn0yyqpo';
+        $this->api_key = '010e7508032e404c54ca850f8da4ea26';
+        $this->api_secret = 'ek/fvO7YanVTgU6shT6eljUaGBQU3VB6ERv1ZfmPYjrTvqUXgg7s8Ng9mOkuuk3rzz7KRbTuyrW7lQNf/8IbGw==';
+        $this->passphrase = 'ngucn0yyqpo';
 
+        $this->setUpConfiguration();
+        $this->setUpClient();
+        
+    }
+
+    private function setUpConfiguration()
+    {
         $this->configuration = Configuration::apiKey($this->api_key, $this->api_secret, $this->passphrase);
+    }
+    
+    private function setUpClient()
+    {
         $this->client = Client::create($this->configuration);
     }
 
@@ -38,6 +48,31 @@ class CoinbaseController extends Controller
         return $this->client->getProductTrades("BTC-USD");
     }
 
+    public function orders()
+    {
+        // dd($this->client->conversion([
+        //     'from' => 'USDC',
+        //     'to' => 'USDC',
+        //     'amount' => 1,
+        // ]));
+
+        return $this->client->getOrders([
+            'status' => 'all',
+        ]);
+
+        // "filled_size": "0.02439452"
+        // "executed_value": "197.4629058112000000"
+        # filled_size -> Valor em BTC
+        # executed_value -> Valor em USDC
+
+        return $this->client->getAccount("d3de80f0-3c8b-4a92-95eb-97602e49817a");
+    }
+
+    public function order($order_id)
+    {
+        return $this->client->getOrder($order_id);
+    }
+
     public function conversor($value, $currency = 'BTC')
     {
         # Place a new order:
@@ -48,14 +83,7 @@ class CoinbaseController extends Controller
         //     "product_id": "BTC-USD"
         // }
         # size: amount of BTC to sell
-        return response()->json([
-            'size'       => $value,
-            'price'      => 0.1,
-            'type'       => 'market',
-            'side'       => 'sell',
-            'product_id' => 'BTC-USD'
-        ]);
-        return $this->client->placeOrder([
+        $response = $this->client->placeOrder([
             'size'       => $value,
             'price'      => 0.1,
             'type'       => 'market',
@@ -63,5 +91,6 @@ class CoinbaseController extends Controller
             'product_id' => 'BTC-USD'
         ]);
 
+        return $response;
     }
 }
--- app/Http/Controllers/ConversorController.php
+++ app/Http/Controllers/ConversorController.php
@@ -14,14 +14,45 @@ class ConversorController extends Controller
         # Validar currency, value
         $data = $request->only('value', 'currency');
 
-        if (strtoupper($data["currency"]) !== "BTC") {
-            return response()->json(['error' => 'Currency not avaible']);
-        }
+        // if (strtoupper($data["currency"]) !== "BTC") {
+        //     return response()->json(['error' => 'Currency not avaible']);
+        // }
 
-        return app($this->coinbaseController)->conversor(0.0000000001);
+        try {
+            $response = app($this->coinbaseController)->conversor($data["value"], $data["currency"]);
+            // dd($response["id"]);
+            sleep(5);
+            dd($this->getOrder($response["id"]));
+            // return $response;
+        } catch(Exception $e) {
+            return response()->json([
+                'error' => 'Error: '.$e->getMessage()
+            ]);
+        }        
+    }
+
+    public function getBtcWallet()
+    {
+        return 'BTC Wallet';
+    }
+
+    public function getUsdcWallet()
+    {
+        return 'USDC Wallet';
+    }
+
+    public function orders()
+    {
+        return app($this->coinbaseController)->orders();
     }
 
     private function x($value): float {
         return floatval($value);
+    }
+
+    private function getOrder($id)
+    {
+        $response = app($this->coinbaseController)->order($id);
+        return $response;
     }
 }
