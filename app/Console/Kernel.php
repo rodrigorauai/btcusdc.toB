15c02c4de5abf1b1809c5a955e1c5f913a851959
Matheus Reis <theus.h.ass.reis@gmail.com>
2019-11-23 14:00:16 -0300

    Code to prevent some errors

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->command('conversor:currency')
+                 ->withoutOverlapping(1)
                  ->everyMinute();
     }
 
